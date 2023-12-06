c93d483f1541574c50f0c5e9319f15ffbfd7514b
oKevinRaupp <rauppke@gmail.com>
2023-12-06 12:54:19 -0300

    Pattern DTO to retur User

--- src/main/java/com/kevinraupp/mongodb/resources/UserResource.java
+++ src/main/java/com/kevinraupp/mongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.kevinraupp.mongodb.resources;
 
 import com.kevinraupp.mongodb.domain.User;
+import com.kevinraupp.mongodb.dto.UserDTO;
 import com.kevinraupp.mongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -19,8 +21,9 @@ public class UserResource {
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
